a2ae7c018236c69ae8b64bbf531c397b7b2c2aa2
heeyeon Kim <hykim01@goability.co.kr>
2024-05-19 01:11:30 +0900

    feat: 로그인 기능 구현 중

--- src/main/java/com/example/fitwithme/application/service/UserService.java
+++ src/main/java/com/example/fitwithme/application/service/UserService.java
@@ -4,15 +4,17 @@ import com.example.fitwithme.common.exception.ErrorStatus;
 import com.example.fitwithme.common.exception.NotFoundException;
 import com.example.fitwithme.domain.model.User;
 import com.example.fitwithme.infrastructure.dao.UserDao;
-import com.example.fitwithme.infrastructure.mapper.UserMapper;
 import com.example.fitwithme.jwt.JwtUtil;
 import com.example.fitwithme.presentation.dto.request.UserRequest;
 import com.example.fitwithme.presentation.dto.response.UserResponse;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static java.rmi.server.LogStream.log;
 
+
+@Slf4j
 @Service
 public class UserService {
     private final UserDao userDao;
@@ -27,6 +29,8 @@ public class UserService {
     public UserResponse login(UserRequest.login loginRequest) {
         String userId = loginRequest.getUserId();
         User user = userDao.findById(userId);
+        log.info("loginRequest : " + loginRequest.getUserPassword());
+        log.info("user : " + user);
         //비밀번호 확인
         if(!loginRequest.getUserPassword().equals(user.userPassword())){
             throw new NotFoundException(ErrorStatus.WRONG_PASSWORD);
---
+++ src/test/java/com/example/fitwithme/presentation/controller/UserControllerTest.java
@@ -0,0 +1,7 @@
+package com.example.fitwithme.presentation.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+}
